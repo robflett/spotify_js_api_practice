4053d0055910bee262d8580e39081599511f7996
Rob <robflettcc@gmail.com>
2017-04-12 17:26:14 +0100

    album names listing

--- public/app.js
+++ public/app.js
@@ -4,4 +4,35 @@ var app = function(){
   makeRequest(url, requestComplete)
 }
 
+var requestComplete = function(){
+  console.log("this is being succesfully called")
+  if (this.status != 200) return; 
+  
+  var jsonString = this.responseText;
+  var catAlbums = JSON.parse(jsonString);
+  console.log(catAlbums);
+  populateList(catAlbums);
+}
+
+var populateList = function(object){
+  var ul = document.querySelector("#albums");
+
+  var catAlbums = object.albums.items;
+
+  catAlbums.forEach(function(catAlbum){
+    var li = document.createElement("li");
+    li.innerText = catAlbum.name;
+
+    // then add the li to the ul
+    ul.appendChild(li);
+  })
+}
+
+var makeRequest = function(url, callback){
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onload = callback;
+  request.send();
+}
+
 window.onload = app;
